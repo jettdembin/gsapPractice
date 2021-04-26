686e52c72b0e273e0d5a4a739cbd4a88e0c4a838
Jett Dembin <71148367+jettdembin@users.noreply.github.com>
2021-04-26 06:13:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ tl.to('.intro', { y: "-100%", duration: 1 }, "-=1.025");
 tl.fromTo('nav', { opacity: 0}, { opacity: 1, duration: 1.25 });
 tl.fromTo('.title', { opacity: 0}, { opacity: 1, duration: 2.5 }, "-=1");
 tl.to('.info-hide', { y: "0%", duration: 1, stagger: 0.5 }, "-=2.5");
-tl.fromTo('img', { opacity: 0}, { opacity: 1, duration: 3 }, "-=2");
+tl.fromTo('img', { opacity: 0}, { opacity: 1, duration: 4 }, "-=2");
+tl.fromTo('img', { x: "100%"}, { x: "0%" , duration: 2}, "-=5");
+tl.fromTo('#logo', { x: "-100%"}, { x: "0%" , duration: 2}, "-=6");
+tl.fromTo('ul', { x: "50%"}, { x: "0%" , duration: 2}, "-=6");
